c9d430ffb81fabf49a6cb5121379eb41ee1b85c4
Саитбаталова Анастасия <akutenkou@internet.ru>
2025-12-17 21:12:33 +0500

    12.10. Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -3,13 +3,25 @@ import { renderThumbnails } from './thumbnails.js';
 import { initUploadForm } from './upload-form.js';
 import { initScaleAndEffects } from './scale-and-effects.js';
 import { showMessage } from './messages.js';
+import { initFilters } from './filters.js';
 
 const pictureContainerElement = document.querySelector('.pictures');
+
+const clearThumbnails = () => {
+  const thumbnailElements = pictureContainerElement.querySelectorAll('.picture');
+  thumbnailElements.forEach((element) => element.remove());
+};
+
+const renderFilteredPhotos = (photos) => {
+  clearThumbnails();
+  renderThumbnails(photos, pictureContainerElement);
+};
 
 const loadAndRenderPhotos = async () => {
   try {
     const photos = await getData();
     renderThumbnails(photos, pictureContainerElement);
+    initFilters(photos, renderFilteredPhotos);
   } catch (error) {
     showMessage('error', error.message);
   }
